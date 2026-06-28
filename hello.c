e3b85b5b5b2f2d4006ece71a683ebfb1cf41255a
weimingming888 <weimingming888@gmail.com>
2026-06-29 01:08:19 +0800

    更新 hello.c

--- hello.c
+++ hello.c
@@ -1,29 +1,25 @@
-ifneq ($(KERNELRELEASE),)
-ccflags-y := -std=gnu11 -ffreestanding -fno-pie -fno-plt
-obj-m += hello.o
-else
-KDIR ?= ./kernel
-ARCH ?= arm64
-CROSS_COMPILE ?=
-LLVM ?= 0
-PWD := $(shell pwd)
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
 
-all:
-ifeq ($(LLVM),1)
-        $(MAKE) -C $(KDIR) \
-        ARCH=$(ARCH) \
-        CC=clang \
-        CXX=clang++ \
-        LD=ld.lld \
-        LLVM=1 \
-        M=$(PWD)
-else
-        $(MAKE) -C $(KDIR) \
-        ARCH=$(ARCH) \
-        CROSS_COMPILE=$(CROSS_COMPILE) \
-        M=$(PWD)
-endif
+// 模块信息
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("test");
+MODULE_DESCRIPTION("Simple test ko module");
+MODULE_VERSION("1.0");
 
-clean:
-        $(MAKE) -C $(KDIR) M=$(PWD) clean
-endif
+// 模块加载入口
+static int __init hello_init(void)
+{
+    pr_info("Hello Kernel Module Loaded!\n");
+    return 0;
+}
+
+// 模块卸载入口
+static void __exit hello_exit(void)
+{
+    pr_info("Hello Kernel Module Unloaded!\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
